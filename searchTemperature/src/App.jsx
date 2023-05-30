b375d1e692bc9bf8318fd5bd259935ed8e5e18bb
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2023-05-30 09:03:56 -0300

    feat: header structure

--- searchTemperature/src/App.jsx
+++ searchTemperature/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BsFillSunFill } from 'react-icons/bs'
+
+import './css/app.css'
 
 function App() {
   return (
     <>
-      <div>
-
-      </div>
+      <main>
+        <header>
+          <div className="container-grid">
+            <form action="">
+              <input type="search" name="search" id="search" placeholder="Search" />
+            </form>
+          </div>
+          <div className="container-flex">
+            <div className='top'>
+              <BsFillSunFill />
+              <p>GOOD MORNING, IT'S CURRENTLY</p>
+            </div>
+            <h1>11:37</h1>
+            <h2>IN LONDON, UK</h2>
+          </div>
+        </header>
+        {/* <div>teste</div> */}
+      </main>
     </>
   )
 }
