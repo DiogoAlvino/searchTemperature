e78c6605a01e5434d71d67bc5de4f7a0044317fe
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2023-11-06 21:11:27 -0300

    .

--- searchTemperature/src/App.jsx
+++ searchTemperature/src/App.jsx
@@ -1,40 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import SearchBar from './components/SearchBar';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import './css/app.css';
-import London from './assets/london.webp'
-import { callApi } from './functions';
-import { setPhrase } from './functions';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import SearchBar from "./components/SearchBar"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import "./css/app.css"
+import London from "./assets/london.webp"
+import { callApi } from "./functions"
+import { setPhrase } from "./functions"
 
 function App() {
-  const [city, setCity] = useState('Marabá');
-  const [selectCity, setSelectCity] = useState('Marabá');
-  const [apiData, setApiData] = useState(null);
-  const [timePhrase, setTimePhrase] = useState("");
+  const [city, setCity] = useState("Marabá")
+  const [selectCity, setSelectCity] = useState("Marabá")
+  const [apiData, setApiData] = useState(null)
+  const [timePhrase, setTimePhrase] = useState("")
 
   useEffect(() => {
     callApi(city, selectCity)
-      .then(response => {
-        setApiData(response);
+      .then((response) => {
+        setApiData(response)
       })
-      .catch(error => {
-        console.log('Error:', error);
-      });
-  }, [city, selectCity]);
+      .catch((error) => {
+        console.log("Error:", error)
+      })
+  }, [city, selectCity])
 
   useEffect(() => {
     if (apiData && apiData.currentTime) {
-      setTimePhrase(setPhrase(apiData.currentTime));
+      setTimePhrase(setPhrase(apiData.currentTime))
     }
-  }, [apiData]);
+  }, [apiData])
 
   return (
     <>
       <main>
-        <img src={London} alt="city view" className='bg-image' />
-        <Header city={city} setCity={setCity} setSelectCity={setSelectCity} apiData={apiData} />
+        <img src={London} alt="city view" className="bg-image" />
+        <Header
+          city={city}
+          setCity={setCity}
+          setSelectCity={setSelectCity}
+          apiData={apiData}
+        />
         <div className="container-flex">
           {apiData && (
             <Hero
@@ -47,7 +52,7 @@ function App() {
         </div>
       </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
